04a9b5c70b8d490d1a7e2ace245cc8466ff52e25
francisco <franads@gmail.com>
2022-06-03 22:03:12 -0300

    incluir conexao com banco

---
+++ AplicacaoWeb/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicacaoWeb.Models
+{
+    public class Produto
+    {
+        public int id {get; set;}
+        public string nomeProduto { get; set; }
+        public string tipoProduto { get; set; }
+    }
+
+    public class ProdutoDbContext : DbContext
+    {
+        DbSet<Produto> produtos { get; set; }
+    }
+}
